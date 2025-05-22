3156e0be5d85210a304febb8df5df86b7bf0b9e2
Gabriel Lima <gabriel58221@gmail.com>
2025-05-22 20:53:52 -0300

    to-do-list

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="bg-black">Welcome to React + TypeScript + Vite</h1>
+      <p>To get started, edit <code>src/App.tsx</code> and save to reload.</p>
+      <p>Learn more about Vite:</p>
+      <ul>
+        <li><a href="https://vitejs.dev/guide/">Vite Guide</a></li>
+        <li><a href="https://vitejs.dev/guide/features.html">Vite Features</a></li>
+        <li><a href="https://vitejs.dev/guide/api-javascript.html">Vite API</a></li>
+      </ul>
+    </>
+  )
+}

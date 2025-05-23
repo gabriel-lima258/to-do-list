84290a2043af425bf140fabf6743edb9a46feb88
Gabriel Lima <gabriel58221@gmail.com>
2025-05-23 20:06:57 -0300

    Refactor TaskItem component to accept task prop and display task title; update TaskList to render tasks dynamically

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -8,9 +8,15 @@ import React from "react";
 import InputText from "../components/input-text";
 import XIcon from "../assets/icons/x.svg?react";
 import CheckIcon from "../assets/icons/check.svg?react";
+import type { Task, TaskState } from "../models/task";
+import { cx } from "class-variance-authority";
 
-export default function TaskItem() {
-	const [isEditing, setIsEditing] = React.useState(false);
+interface TaskItemProps {
+	task: Task;
+}
+
+export default function TaskItem({ task }: TaskItemProps) {
+	const [isEditing, setIsEditing] = React.useState(task?.state === "creating" as TaskState);
 
 	function handleEditTask() {
 		setIsEditing(true);
@@ -19,13 +25,22 @@ export default function TaskItem() {
 	function handleExitTask() {
 		setIsEditing(false);
 	}
-  
+
 	return (
 		<Card size={"md"} className="flex items-center gap-4">
 			{!isEditing ? (
 				<>
-					<InputCheckbox />
-					<Text className="flex-1"> Fazer compras da semana</Text>
+					<InputCheckbox
+						value={task?.concluded?.toString()}
+						checked={task?.concluded}
+					/>
+					<Text
+						className={cx("flex-1", {
+							"line-through": task?.concluded,
+						})}
+					>
+						{task?.title}
+					</Text>
 					<div className="flex gap-1">
 						<ButtonIcon icon={TrashIcon} variant={"tertiary"} />
 						<ButtonIcon
@@ -39,10 +54,11 @@ export default function TaskItem() {
 				<>
 					<InputText className="flex-1" />
 					<div className="flex gap-1">
-						<ButtonIcon 
-            icon={XIcon}
-             variant={"secondary"}
-             onClick={handleExitTask} />
+						<ButtonIcon
+							icon={XIcon}
+							variant={"secondary"}
+							onClick={handleExitTask}
+						/>
 						<ButtonIcon icon={CheckIcon} variant={"primary"} />
 					</div>
 				</>
--- src/core-components/task-list.tsx
+++ src/core-components/task-list.tsx
@@ -5,26 +5,25 @@ import useTasks from "../hooks/use-tasks";
 import useTask from "../hooks/use-task";
 
 export default function TaskList() {
-  const { tasks } = useTasks();
-  const { prepareTask } = useTask();
+	const { tasks } = useTasks();
+	const { prepareTask } = useTask();
 
-  function handleNewTask() {
-    prepareTask();
-  }
+	function handleNewTask() {
+		prepareTask();
+	}
 
-  return (
-    <>
-      <section>
-        <Button icon={PlusIcon} className="w-full" onClick={handleNewTask}>
-          Nova tarefa
-        </Button>
-      </section>
-      <section className="space-y-2">
-        <TaskItem/>
-        <TaskItem/>
-        <TaskItem/>
-        <TaskItem/>
-      </section>
-    </>
-  )
+	return (
+		<>
+			<section>
+				<Button icon={PlusIcon} className="w-full" onClick={handleNewTask}>
+					Nova tarefa
+				</Button>
+			</section>
+			<section className="space-y-2">
+				{tasks.map((task) => (
+					<TaskItem key={task.id} task={task}/>
+				))}
+			</section>
+		</>
+	);
 }
